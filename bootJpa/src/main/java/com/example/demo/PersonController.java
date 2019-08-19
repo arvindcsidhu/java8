0545f44ec2ed3fd1cd4f952d45a934165ac56170
sidhu chandrappa <sidhu chandrappa@192.168.1.9>
2019-08-19 21:46:37 +0530

    file name changes

--- bootJpa/src/main/java/com/example/demo/PersonController.java
+++ bootJpa/src/main/java/com/example/demo/PersonController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HomeController {
+public class PersonController {
 	
 	@Autowired
 	 PersonRepository repo;
